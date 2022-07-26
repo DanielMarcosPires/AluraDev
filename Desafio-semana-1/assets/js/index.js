7168689bc0bc0005d1c4bd9e6ff7ee924e910a1b
Daniel Marcos Pires <89848369+DanielMarcosPires@users.noreply.github.com>
2022-07-26 19:42:28 -0300

    Add files via upload

---
+++ Desafio-semana-1/assets/js/index.js
@@ -0,0 +1,13 @@
+const menushow = document.querySelector('#show-menu')
+
+const menu = document.querySelector('.menu')
+menushow.addEventListener('click',()=>{
+    menu.classList.toggle('menu-active')
+})
+
+const search = document.querySelector('#icon-search')
+
+const searchbar = document.querySelector('form .search')
+search.addEventListener('click',()=>{
+   
+})
